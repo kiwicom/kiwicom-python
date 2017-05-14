92c0401d6a43f3ab5359e8828597037a6cfdd588
Andrii <blayson4@gmail.com>
2017-05-15 01:26:24 +0200

    Updated logger and added 2 methods
    Added config and flights_multi method,
    reworked logger

--- kiwiwrapper/kiwi.py
+++ kiwiwrapper/kiwi.py
@@ -1,18 +1,45 @@
+import datetime
 import logging
 import sys
-import time
+import os
+import configparser
 
+from structlog import get_logger
+from structlog import configure
+from structlog import processors
+from structlog import stdlib
 import requests
-import dicttoxml
+from pythonjsonlogger import jsonlogger
 
 # Just for pretty printing
 from xml.dom.minidom import parseString
 import pprint
 
-try:
-    import lxml.etree as etree
-except ImportError:
-    import xml.etree.ElementTree as etree
+
+def configure_logger(log_level, log_file):
+    configure(
+        processors=[
+            stdlib.filter_by_level,
+            stdlib.add_logger_name,
+            stdlib.add_log_level,
+            stdlib.PositionalArgumentsFormatter(),
+            processors.StackInfoRenderer(),
+            processors.format_exc_info,
+            processors.UnicodeDecoder(),
+            stdlib.render_to_log_kwargs,
+        ],
+        context_class=dict,
+        logger_factory=stdlib.LoggerFactory(),
+        wrapper_class=stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+    handler = logging.FileHandler(filename=log_file)
+    handler.setFormatter(jsonlogger.JsonFormatter('%(asctime)s %(filename)s '
+                                                  '%(lineno)d %(message)s'))
+    logger = get_logger(__name__)
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+    return logger
 
 
 class UnexpectedParameter(KeyError):
@@ -31,53 +58,40 @@ class Transport(object):
     """
     Parent class for initialisation
     """
-    API_HOST = 'https://api.skypicker.com'
     _FORMATS = ('json', 'xml')
-    _TIME_ZONES = ('utc', 'gmt')
+    _TIME_ZONES = 'gmt'
     _LOG_FORMATS = ('DEBUG', 'INFO', 'WARN', 'WARNING', 'ERROR', 'CRITICAL')
 
-    def __init__(self, log='WARN', log_file='logs.log', time_zone='utc'):
+    def __init__(self, log_level='WARNING', log_file='logs.log',
+                 time_zone='gmt', cfg='apiconfig.ini'):
         """
-        :param log: 
+        :param log_level: 
         :param log_file: 
         :param time_zone: 
         """
-        # if response_format.lower() not in self._FORMATS:
-        #     raise ValueError(
-        #         'Unknown response format: "{}", supported formats are {}'.format(response_format,
-        #                                                                          ', '.join(self._FORMATS))
-        #     )
         if time_zone.lower() not in self._TIME_ZONES:
             raise ValueError(
-                'Unknown time zone: {}, supported time zones are {}'.format(time_zone,
-                                                                            ', '.join(self._TIME_ZONES))
+                'Unknown time zone: {}, '
+                'supported time zones are {}'
+                ''.format(time_zone, ', '.join(self._TIME_ZONES))
             )
-        if log.upper() not in self._LOG_FORMATS:
+        if log_level.upper() not in self._LOG_FORMATS:
             raise ValueError(
-                'Unknown log format: {}, supported log formats are {}'.format(log,
-                                                                              ', '.join(self._LOG_FORMATS))
+                'Unknown log format: {}, '
+                'supported log formats are {}'
+                ''.format(log_level, ', '.join(self._LOG_FORMATS))
             )
         self.time_settings = time_zone.lower()
-        # self.response_format = response_format.lower()
-        self.log = getattr(logging, log.upper())
-        self.log_file = log_file
+        self._read_cfg(cfg)
+        self.API_HOST = os.environ['API_HOST']
+        self.log = configure_logger(log_level=log_level.upper(),
+                                    log_file=log_file)
 
-    def configure_logger(self, log_level=logging.WARN):
-        logger = logging.getLogger(__name__)
-        logger.setLevel(log_level)
-        formatter = logging.Formatter('%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s')
-        # Setup console logging
-        sh = logging.StreamHandler(stream=sys.stdout)
-        sh.setFormatter(formatter)
-        logger.addHandler(sh)
-        # Setup file logging
-        fh = logging.FileHandler(filename=self.log_file, encoding='utf-8')
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-        return logger
-
-    def make_request(self, service_url, params, method='get', headers=None, data=None, callback=None):
+    def make_request(self, service_url, params, method='get', headers=None,
+                     data=None, stream=False, json_data=None, callback=None):
         """
+        :param json_data: 
+        :param stream: 
         :param service_url: 
         :param method: 
         :param headers: 
@@ -89,16 +103,13 @@ class Transport(object):
         if callback is None:
             callback = self._default_callback
 
+        self.log.debug('Request', URL=service_url, method=method.upper(),
+                       params=params, headers=headers)
+
         request = getattr(requests, method.lower())
+        r = request(service_url, params=params, headers=headers, data=data,
+                    stream=stream, json=json_data)
 
-        log = self.configure_logger(log_level=self.log)
-        log.debug('| Request URL: %s' % service_url)
-        log.debug('| Request method: %s' % method.upper())
-        log.debug('| Request query params: %s' % params)
-        log.debug('| Request headers: %s' % headers)
-
-        r = request(service_url, headers=headers, data=data, params=params)
-        log.info('| Full Request URL: %s' % r.url)
         try:
             r.raise_for_status()
             return callback(r)
@@ -126,8 +137,21 @@ class Transport(object):
     #         return response
 
     @staticmethod
-    def _error_handling():
-        pass
+    def _params_maker(params, req_params=None):
+        for (key, value) in params.items():
+            req_params[key] = value
+        return req_params
+
+    @staticmethod
+    def _read_cfg(cfg_file):
+        config = configparser.ConfigParser()
+        config.read(cfg_file)
+        host = config['DEFAULT']['api-host']
+        os.environ['API_HOST'] = host
+
+    @staticmethod
+    def _error_handling(response):
+        return response
 
     @staticmethod
     def _default_callback(response):
@@ -141,6 +165,7 @@ class Search(Transport):
     """
     Search Class
     """
+
     def places(self, **params):
         """
         Get request with params to api.skypicker.com/places
@@ -155,15 +180,17 @@ class Search(Transport):
         )
         :return: Json of skypicker api ids
         """
-        required_params = {}
-        for (key, value) in params.items():
-            required_params[key] = value
         service_url = "{API_HOST}/places".format(API_HOST=self.API_HOST)
-        return self.make_request(service_url, params=required_params)
+        return self.make_request(service_url,
+                                 params=params)
 
-    def search_flights(self, flyFrom, partner_market, dateFrom, dateTo, partner='picky', **params):
+    def search_flights(self, fly_from, partner_market, date_from, date_to,
+                       partner='picky', headers=None, stream=False, **params):
         """  
-        :param flyFrom: Skypicker api id of the departure destination. Accepts multiple values separated by comma, 
+        :param stream: 
+        :param headers: 
+        :param fly_from: Skypicker api id of the departure destination. 
+                Accepts multiple values separated by comma, 
                 these values might be airport codes, city IDs, two letter country codes, 
                 metropolitan codes and radiuses. Radius needs to be in form lat-lon-xkm. 
                 E.g. -23.24--47.86-500km for places around Sao Paulo, LON - checks every airport in London, 
@@ -173,16 +200,18 @@ class Search(Transport):
         :param partner_market: Market from which the request is coming from. 
                 Example: us.
                 [String]
-        :param dateFrom: search flights from this date (dd/mm/YYYY). Use parameters dateFrom and dateTo as a date range 
+        :param date_from: search flights from this date (dd/mm/YYYY). 
+                Use parameters dateFrom and dateTo as a date range 
                 for the flight departure. Parameter dateFrom 01/05/2016 and dateTo 30/05/2016 means, 
                 that the departure can be anytime between those dates. For the dates of the return flights, 
                 use the returnTo&returnFrom or daysInDestinationFrom & daysInDestinationTo parameters 
                 Example: 08/08/2017.
                 [String]
-        :param dateTo: search flights until this date (dd/mm/YYYY) 
+        :param date_to: search flights until this date (dd/mm/YYYY) 
                 Example: 08/09/2017.
                 [String]
-        :param partner: partner ID. If present, in the result will be also a link to a specific trip directly 
+        :param partner: partner ID. 
+                If present, in the result will be also a link to a specific trip directly 
                 to kiwi.com, with the affiliate id included (use picky partner ID for testing) 
                 Example: picky.
                 [String]
@@ -190,16 +219,41 @@ class Search(Transport):
         :return: response with JSON or XML content
         """
         required_params = {
-            'flyFrom': flyFrom,
-            'dateFrom': dateFrom,
-            'dateTo': dateTo,
+            'flyFrom': fly_from,
+            'dateFrom': date_from,
+            'dateTo': date_to,
             'partner': partner,
             'partner_market': partner_market
         }
-        for (key, value) in params.items():
-            required_params[key] = value
+
         service_url = "{API_HOST}/flights".format(API_HOST=self.API_HOST)
-        return self.make_request(service_url, params=required_params)
+        return self.make_request(service_url,
+                                 params=self._params_maker(params=params,
+                                                           req_params=required_params),
+                                 headers=headers,
+                                 stream=stream)
+
+    def search_flights_multi(self, json_data=None, data=None,
+                             headers=None, **params):
+        """
+        
+        :param json_data: 
+        :param data: 
+        :param headers: 
+        :param params:
+        :required_data: 
+        (
+            affilid: Your partner ID, for testing use 'picky'
+        )
+        :return: 
+        """
+        service_url = "{API_HOST}/flights_multi".format(API_HOST=self.API_HOST)
+        return self.make_request(service_url,
+                                 params=params,
+                                 method='post',
+                                 json_data=json_data,
+                                 data=data,
+                                 headers=headers)
 
 
 class Booking(Transport):
@@ -208,9 +262,21 @@ class Booking(Transport):
     """
     pass
 
+
 if __name__ == '__main__':
-    s = Search(log='info', log_file='info.log')
-    # res = s.places(id='SK', term='br', bounds='lat_lo,lat_hi')
-    res1 = s.search_flights(flyFrom='CZ', dateFrom='03/05/2017', dateTo='13/05/2017', partner_market='US')
+    s = Search(log_level='debug', log_file='info.log')
+    # res = s.places(id='SK', term='br', bounds='lat_lo,lat_hi', headers='Accept: application/xml')
+    res1 = s.search_flights(fly_from='CZ', date_from='03/05/2017', date_to='13/05/2017', partner_market='US')
+    v = {"requests": [
+        {"v": 2, "sort": "duration", "asc": 1, "locale": "en", "daysInDestinationFrom": "", "daysInDestinationTo": "",
+         "affilid": "picky", "children": 0, "infants": 0, "flyFrom": "BRQ", "to": "BCN", "featureName": "results",
+         "dateFrom": "09/05/2017", "dateTo": "09/06/2017", "typeFlight": "oneway", "returnFrom": "", "returnTo": "",
+         "one_per_date": 0, "oneforcity": 0, "wait_for_refresh": 0, "adults": 1},
+        {"v": 2, "sort": "duration", "asc": 1, "locale": "en", "daysInDestinationFrom": "", "daysInDestinationTo": "",
+         "affilid": "picky", "children": 0, "infants": 0, "flyFrom": "BCN", "to": "ZAG", "featureName": "results",
+         "dateFrom": "12/06/2017", "dateTo": "15/06/2017", "typeFlight": "oneway", "returnFrom": "", "returnTo": "",
+         "one_per_date": 0, "oneforcity": 0, "wait_for_refresh": 0, "adults": 1}], "limit": 45}
+    # res2 = s.search_flights_multi(json_data=v)
     # print(parseString(res1).toprettyxml())
+    # pprint.pprint(res2.json())
     pprint.pprint(res1.json())
